45f69dce06d6a99eb52d571002eb3920c0b4994a
Viliprant <viliprant@gmail.com>
2022-08-01 23:03:19 +0200

    fix view list transaction

--- src/app/mocks/transaction.mocks.ts
+++ src/app/mocks/transaction.mocks.ts
@@ -3,18 +3,90 @@ import { PaymentEnum, Transaction } from "../types/transaction";
 export const transactionsMock: Transaction[] = [
     {
         id: 1,
-        amount: 12,
+        amount: 70,
         date: new Date(),
-        labelPayment: 'test',
+        labelPayment: 'Happy Birthday',
         partnership: 'Eudrey',
         paymentType: PaymentEnum.INCOME
     },
     {
         id: 2,
-        amount: 100,
+        amount: 13.40,
         date: new Date(),
-        labelPayment: 'test',
-        partnership: 'Eudrey',
+        labelPayment: 'Burger King',
+        partnership: 'Jo',
         paymentType: PaymentEnum.INCOME
+    },
+    {
+        id: 3,
+        amount: 900,
+        date: new Date(),
+        labelPayment: 'Salary',
+        partnership: 'Jo',
+        paymentType: PaymentEnum.INCOME
+    },
+    {
+        id: 3,
+        amount: 900,
+        date: new Date(),
+        labelPayment: 'Salary',
+        partnership: 'Jo',
+        paymentType: PaymentEnum.INCOME
+    },
+    {
+        id: 3,
+        amount: 900,
+        date: new Date(),
+        labelPayment: 'Salary',
+        partnership: 'Jo',
+        paymentType: PaymentEnum.INCOME
+    },
+    {
+        id: 3,
+        amount: 900,
+        date: new Date(),
+        labelPayment: 'Salary',
+        partnership: 'Jo',
+        paymentType: PaymentEnum.INCOME
+    },
+    {
+        id: 3,
+        amount: 900,
+        date: new Date(),
+        labelPayment: 'Salary',
+        partnership: 'Jo',
+        paymentType: PaymentEnum.INCOME
+    },
+    {
+        id: 3,
+        amount: 900,
+        date: new Date(),
+        labelPayment: 'Salary',
+        partnership: 'Jo',
+        paymentType: PaymentEnum.INCOME
+    },
+    {
+        id: 3,
+        amount: 900,
+        date: new Date(),
+        labelPayment: 'Salary',
+        partnership: 'Jo',
+        paymentType: PaymentEnum.INCOME
+    },
+    {
+        id: 4,
+        amount: 28.90,
+        date: new Date(),
+        labelPayment: 'BK',
+        partnership: 'Burger King',
+        paymentType: PaymentEnum.OUTCOME
+    },
+    {
+        id: 5,
+        amount: 75,
+        date: new Date(),
+        labelPayment: 'Navigo',
+        partnership: 'Transport',
+        paymentType: PaymentEnum.OUTCOME
     }
 ]

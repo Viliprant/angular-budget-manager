8aba7388e4c3455eb4adc77d387607bb172dc39d
Eudrey Bandelaria <eudreybandelaria@gmail.com>
2022-08-02 00:07:20 +0200

    WIP dashboard

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  public welcomeText: string;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
   }
 
+  public ngOnInit(): void {
+    this.setWelcomingText();
+  }
+
+  private setWelcomingText(): void {
+    const today = new Date();
+    const currentHour = +today.toString().split(' ')[4].split(':')[0];
+    this.welcomeText = this.getWelcomingText(currentHour);
+  }
+
+  private getWelcomingText(hour: number): string {
+    if (hour > 16) {
+      return 'Good evening';
+    } else if (hour > 12) {
+      return 'Good afternoon';
+    } else if (hour > 8) {
+      return 'Good morning';
+    } else {
+      return 'You need to sleep';
+    }
+  }
 }
